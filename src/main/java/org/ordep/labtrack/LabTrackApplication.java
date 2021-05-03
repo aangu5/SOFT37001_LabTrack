ad82b12db9172cca7addbe02fb48a9f1c15d01ee
aangu5 <a.cheney5@gmail.com>
2021-05-03 20:02:51 +0100

    Initial Spring Boot commit

---
+++ src/main/java/org/ordep/labtrack/LabTrackApplication.java
@@ -0,0 +1,13 @@
+package org.ordep.labtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ordep/labtrack/LabTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ordep.labtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
