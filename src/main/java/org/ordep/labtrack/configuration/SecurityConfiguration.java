f2eaa3c753fff1f7d4cdb5bcab06b5abea05d4b1
Angus Cheney <a.cheney5@gmail.com>
2021-05-12 23:32:25 +0100

    Favicon attempt 1

--- src/main/java/org/ordep/labtrack/configuration/SecurityConfiguration.java
+++ src/main/java/org/ordep/labtrack/configuration/SecurityConfiguration.java
@@ -30,6 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers("/favicon.ico").permitAll()
                 .antMatchers("/login*").permitAll()
                 .anyRequest().authenticated()
                 .and()
