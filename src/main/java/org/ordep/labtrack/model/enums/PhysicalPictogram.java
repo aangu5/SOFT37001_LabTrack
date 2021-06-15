29ae1388e710a412ff3515822e854c4f86d56bf0
aangu5 <a.cheney5@gmail.com>
2021-06-15 21:47:13 +0100

    Updated enum values

--- src/main/java/org/ordep/labtrack/model/enums/PhysicalPictogram.java
+++ src/main/java/org/ordep/labtrack/model/enums/PhysicalPictogram.java
@@ -6,13 +6,13 @@ public enum PhysicalPictogram {
     PHS03("Radio"),
     PHS04("Ice"),
     PHS05("Magnets"),
-    PHS06("Breath a bit"),
+    PHS06("Inhalation"),
     PHS07("Nuclear"),
     PHS08("Trip Hazard"),
     PHS09("Fall Hazard"),
     PHS10("Risk of Shock"),
     PHS11("Warning"),
-    PHS12("Break up?!");
+    PHS12("Explosive");
 
     private final String displayName;
 
